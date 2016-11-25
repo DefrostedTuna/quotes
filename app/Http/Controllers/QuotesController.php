1ea47663e04d2a6b587ebd3ce8601a106b79f9b7
DefrostedTuna <rbennett1106@gmail.com>
2016-11-25 10:27:51 -0500

    Changed Quotes pagination to 25.

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -10,7 +10,7 @@ class QuotesController extends Controller
 {
     public function getIndex(Quote $quote)
     {
-        $quotes = $quote->orderBy('created_at', 'desc')->paginate(15);
+        $quotes = $quote->orderBy('created_at', 'desc')->paginate(25);
 
         return view('quotes.index')
             ->with('quotes', $quotes);
